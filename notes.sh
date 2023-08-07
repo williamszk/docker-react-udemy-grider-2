710131fafdd4cdab9776b0905f95dc84934037c5
williamszk <wllmszk@gmail.com>
2023-08-07 21:50:52 +0000

    new test with elastic beanstalk

--- notes.sh
+++ notes.sh
@@ -1,4 +1,5 @@
 
+# -----------------------------------------------------------------------------
 # copy some files from the previous videos
 cd ..
 docker-react-udemy-grider-2
@@ -14,3 +15,26 @@ npm run start
 npm run test
 docker build -t williamszk/docker-react-udemy-grider-2 -f Dockerfile.dev .
 docker run -e CI=true williamszk/docker-react-udemy-grider-2 npm run test
+
+# -----------------------------------------------------------------------------
+# to serve the built react app
+npm install -g serve
+serve build
+
+# -----------------------------------------------------------------------------
+# build script that creates the .zip bundle to be sent to an EB running docker
+rm -rf bundle_aws_eb
+mkdir bundle_aws_eb
+cp -r public bundle_aws_eb/
+cp -r src bundle_aws_eb/
+cp -r docker-compose.yml bundle_aws_eb/
+cp -r Dockerfile bundle_aws_eb/
+cp -r package.json bundle_aws_eb/
+cp -r package-lock.json bundle_aws_eb/
+
+rm bundle_aws_eb.zip
+(cd bundle_aws_eb && zip -r ../bundle_aws_eb.zip .)
+
+
+# -----------------------------------------------------------------------------
+
